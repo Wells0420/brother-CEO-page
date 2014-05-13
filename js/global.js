8cbb4c0010b3c69245c6b22a9bac91a0a4f16fab
Wells <lohaswells@gmail.com>
2014-05-13 17:48:29 +0800

    整理文件测试

--- js/global.js
+++ js/global.js
@@ -1,12 +1,11 @@
 // JavaScript Document
 
 // global 
-var $window = $(window),
-    windowHeight = $window.height(),
-	windowWidth = $window.width(),
-	$siteTop,
-	contentH = 1600,
+var $window      	 = $(window),
+	windowHeight     = $window.height(),
+	windowWidth      = $window.width(),
+	$siteTop         = 0,
+	contentH         = 1600,
 	parallaxContentY = (contentH - windowHeight) / 2,
-	homeHRatio = 1.2;
-	
-var screenSizeRatio = 1;
+	homeHRatio       = 1.2,
+	screenSizeRatio  = 1;
